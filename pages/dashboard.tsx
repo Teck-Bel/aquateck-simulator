bc91162bed3a131e6410a2115e63f2bd232f5751
skyteck-belgium <skyteck.belgium@gmail.com>
2025-06-07 16:43:59 +0200

    Create dashboard.tsx

---
+++ pages/dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard AquaTeck Simulator</h1>
+      <p>Hier komt straks de simulator en de data-overzicht.</p>
+    </>
+  );
+}
