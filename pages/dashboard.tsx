33dbd4ab23bde457d00c4371fa6b62c77287dffa
skyteck-belgium <skyteck.belgium@gmail.com>
2025-06-07 18:33:02 +0200

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,14 +3,22 @@ import SensorChart from "../components/SensorChart";
 import ScenarioSelector from "../components/ScenarioSelector";
 import { generateData } from "../utils/simulationEngine";
 
+const regions=["North Sea", "Mediterranean Sea", "Tropical Sea"] as const;
+const seasons=["Spring", "Summer", "Autumn", "Winter"] as const;
+
+type Region = typeof regions[number];
+type Season = typeof seasons[number];
+
 export default function Dashboard() {
   const [data, setData] = useState<{ time: string; value: number }[]>([]);
   const [selectedScenario, setSelectedScenario] = useState("Calm Waters");
+  const [selectedRegion, setSelectedRegion] = useState<Region>("North Sea");
+  const [selectedSeason, setSelectedSeason] = useState<Season>("Summer")
 
   useEffect(() => {
     const interval = setInterval(() => {
       setData((oldData) => {
-        const nextValue = generateData(selectedScenario);
+        const nextValue = generateData(selectedScenario, selectedRegion, selectedSeason);
         const nextTime = new Date().toLocaleTimeString();
         const newData = [...oldData, { time: nextTime, value: nextValue }];
 
@@ -23,23 +31,45 @@ export default function Dashboard() {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [selectedScenario]);
+  }, [selectedScenario, selectedRegion, selectedSeason]);
 
   return (
     <>
       <h1>Dashboard Simulator</h1>
+      
       <ScenarioSelector
         selectedScenario={selectedScenario}
         onChange={setSelectedScenario}
       />
-      <SensorChart
-  title="Water Temperature"
-  unit="°C"
-  currentValue={data.length > 0 ? data[data.length - 1].value : 0}
-  data={data}
-  xAxisLabel="Time (hh:mm:ss)"
-  yAxisLabel="Temperature"
-/>
+
+      <label>
+        Region:
+        <select
+          value={selectedRegion}
+          onChange={(e) => setSelectedRegion(e.target.value as Region)}
+          >
+          {regions.map((r) => (
+            <option key{r} value{}>
+              {r}
+              </option>
+            ))}
+        </select>
+      </label>
+
+      <label style={{ marginLeft:"1rem" }}>
+        Seaons
+        <select
+          value=(selectedSeason}
+        onChange={(e) => setSelectedSeasons(e.target.value as Season)}
+        >
+        {Season.map((s) => (
+          <option key={s} value={s}>
+            {s}
+          </option>
+      ))}
+        </select>
+      </label>
+      <SensorChart data={data />
     </>
   );
 }
