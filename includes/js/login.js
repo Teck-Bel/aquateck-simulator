cbe3a9c4a5e327d00a5c3537ebac6a152b12d472
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-15 15:20:56 +0100

    Create login.js

---
+++ includes/js/login.js
@@ -0,0 +1,26 @@
+Console.log("login.js geladen");
+
+import { auth } from "/aquateck-simulator/includes/firebase/condig.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+  const errorE1 = document.getElementById("error");
+  
+  if (!form) return;
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = "/teck-staff/dashboard.html";
+    } catch (err) {
+      errorE1.textContent = "Ongeldige inloggegevens";
+      console.error(err.code);
+    }
+  });
+});
