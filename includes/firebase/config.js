3f6dd7174de0d95fec7de9f7090ad1ae69e07171
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-21 14:11:41 +0100

    Update config.js

--- includes/firebase/config.js
+++ includes/firebase/config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyANlFkNlU_MB1YA5I6dIh2E4QuCQ_g0eHM",
@@ -9,12 +8,10 @@ const firebaseConfig = {
   projectId: "aquateck-sim",
   storageBucket: "aquateck-sim.firebasestorage.app",
   messagingSenderId: "286597592168",
-  appId: "1:286597592168:web:1b6124ce6736150c7cb7dd",
-  measurementId: "G-SVM6L2SKG9"
+  appId: "1:286597592168:web:1b6124ce6736150c7cb7dd"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
